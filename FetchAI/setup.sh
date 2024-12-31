f816f39ab9f9094cf68c3513f5928909b4aab5d3
ImStaked <82274336+ImStaked@users.noreply.github.com>
2024-12-30 23:32:58 -0600

    seeds

--- FetchAI/setup.sh
+++ FetchAI/setup.sh
@@ -49,11 +49,11 @@ wget https://snapshots.polkachu.com/snapshots/fetch/fetch_19884219.tar.lz4 -O - 
 curl https://raw.githubusercontent.com/fetchai/genesis-fetchhub/fetchhub-4/fetchhub-4/data/genesis_migrated_5300200.json --output ~/.fetchd/config/genesis.json
 
 
-# Customize app.toml
+# edit app.toml
 sed -i "s|minimum-gas-prices = \"\"|minimum-gas-prices = \"0.002afet\"|g"  ~/.fetchd/config/app.toml
 sed -i "s|pruning = \"default\"|pruning = \"everything\"|g"  ~/.fetchd/config/app.toml
 
-# Customize config.toml
+# State Sync
 SNAP_RPC="https://rpc-fetchhub.fetch.ai:443,https://fetch-rpc.polkachu.com:443"
 
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
@@ -65,5 +65,7 @@ s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.fetchd/config/config.toml
 
+# edit config.toml
+sed -i "s|seeds = \"\"|seeds = \"17693da418c15c95d629994a320e2c4f51a8069b@connect-fetchhub.fetch.ai:36456,a575c681c2861fe945f77cb3aba0357da294f1f2@connect-fetchhub.fetch.ai:36457,d7cda986c9f59ab9e05058a803c3d0300d15d8da@connect-fetchhub.fetch.ai:36458\"|g"  ~/.fetchd/config/config.toml
 sed -i "s|prometheus = false|prometheus = true|g"  ~/.fetchd/config/config.toml
 sed -i "s|prometheus_listen_addr = \":26660\"|prometheus_listen_addr = \"$PRIVATE_IP:26660\"|g"  ~/.fetchd/config/config.toml
